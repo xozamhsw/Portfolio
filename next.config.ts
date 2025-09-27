c216464f9208f6d3303aaefc010e4d93c83d866c
xozamhsw <zagarmhsw@gmail.com>
2025-09-27 13:29:25 +0700

    update:config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["i.pinimg.com", "images.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // artinya semua host HTTPS diizinkan
+        pathname: "/**", // semua path diizinkan
+      },
+      {
+        protocol: "http",
+        hostname: "**", // kalau ada gambar non-https
+        pathname: "/**",
+      },
+    ],
   },
 };
 
